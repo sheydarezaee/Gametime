fc0a48f931fb883bbe4663f3fd25c1c68db194a1
Sheyda Rezaei <sheyda.rezaei@bbva.com>
2021-05-31 20:50:57 -0700

    Update api call

--- src/api/yelp.js
+++ src/api/yelp.js
@@ -1,8 +1,8 @@
 import axios from 'axios'
-import { YELP_API_KEY } from '../support/constants'
+import { baseUrl, YELP_API_KEY } from '../support/constants'
 
 export default axios.create({
-  baseURL : 'https://api.yelp.com/v3/businesses',
+  baseURL : `${baseUrl}`,
   headers : {
     'Authorization': `Bearer ${YELP_API_KEY}`,
     'X-Requested-With': 'XMLHttpRequest',
